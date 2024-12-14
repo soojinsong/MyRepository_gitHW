efa6648cb9d2fcb3b3d3eed92f9f056192a139c2
soojinsong <162791828+soojinsong@users.noreply.github.com>
2024-12-15 00:12:22 +0900

    First commit in devB branch

--- GitHW/src/gitHW/A.java
+++ GitHW/src/gitHW/A.java
@@ -1,10 +1,14 @@
 package gitHW;
 
+import gitHW.B;
+
 public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHW!");
 		System.out.println("This is a.");
+		B your = new B();
+		your.sayHello("BBBB");
 	}
 
 }
--- GitHW/src/gitHW/B.java
+++ GitHW/src/gitHW/B.java
@@ -3,6 +3,7 @@ package gitHW;
 public class B {
 	public void sayHello(String towhom) {
 		System.out.println("Hello!" + towhom);
-		System.out.println("This is b.");
+		System.out.println("This is B.");
+		System.out.println("This is class B.");
 	}
 }
