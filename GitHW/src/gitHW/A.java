1fb80296579fb1d1d46717cc1236427a86e1326e
soojinsong <162791828+soojinsong@users.noreply.github.com>
2024-12-15 00:14:03 +0900

    First commit in devC branch

--- GitHW/src/gitHW/A.java
+++ GitHW/src/gitHW/A.java
@@ -1,10 +1,14 @@
 package gitHW;
 
+import gitHW.C;
+
 public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHW!");
 		System.out.println("This is a.");
+		C your = new C();
+		your.sayHello("CCCC");
 	}
 
 }
--- GitHW/src/gitHW/C.java
+++ GitHW/src/gitHW/C.java
@@ -3,6 +3,7 @@ package gitHW;
 public class C {
 	public void sayHello(String towhom) {
 		System.out.println("Hello!" + towhom);
-		System.out.println("This is c.");
+		System.out.println("This is C.");
+		System.out.println("This is class C.");
 	}
 }
