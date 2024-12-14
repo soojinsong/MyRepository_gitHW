ba38d16c969ed865519b7c312d0bcb10459cf73e
soojinsong <162791828+soojinsong@users.noreply.github.com>
2024-12-15 00:16:04 +0900

    Update class A

--- GitHW/src/gitHW/A.java
+++ GitHW/src/gitHW/A.java
@@ -4,7 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHW!");
-		System.out.println("This is a.");
+		System.out.println("This is A.");
+		System.out.println("This is class A.");
 	}
 
 }
